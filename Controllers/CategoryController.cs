f579942c15046e314e15675bcc707dab5b6be550
asirialwis <asirichathurika900@gmail.com>
2024-07-04 19:01:02 +0530

    delete option added

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -80,5 +80,27 @@ namespace BookWeb.Controllers
 
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var catID = _db.Categories.Find(id);
+            return View(catID);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id){
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
